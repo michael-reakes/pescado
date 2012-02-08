59ddb8a6630b3407ac78556707b7f7d6d762340a
Michael Reakes <michael.reakes@gmail.com>
2012-02-08 15:32:57 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pescado::Application.config.secret_token = 'b3cdebebff0d091e5751c117f31bd53cca71ed4f5ea7586ff90d179ce4ac04590edb60ecfb537f67540c429445a881bab5082f6f1ed93c60d3d779ab1b3c21a1'
