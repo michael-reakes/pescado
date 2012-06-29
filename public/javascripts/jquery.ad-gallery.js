3701417be3bf6e44652558837678ab633d5eccbd
Michael Reakes <michael.reakes@gmail.com>
2012-06-29 14:10:19 +1000

    Screen resolution.

--- public/javascripts/jquery.ad-gallery.js
+++ public/javascripts/jquery.ad-gallery.js
@@ -9,7 +9,7 @@
  */
 (function($) {
   $.fn.adGallery = function(options) {
-    var defaults = { loader_image: 'loader.gif',
+    var defaults = { loader_image: '../images/loader.gif',
                      start_at_index: 0,
                      update_window_hash: true,
                      description_wrapper: false,
